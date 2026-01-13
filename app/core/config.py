b524da6731fcb1bd3fc1fb88787a0f5d9ffad6f2
Sundarr Baror <sundarrbaror@sundarbaror.local>
2026-01-13 19:17:15 +0530

    upodated

--- app/core/config.py
+++ app/core/config.py
@@ -1,10 +1,18 @@
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
+# import os
+
+# load_dotenv()  # load .env file
+
+# SUPABASE_URL = os.getenv("NEXT_PUBLIC_SUPABASE_URL")
+# SUPABASE_ANON_KEY = os.getenv("NEXT_PUBLIC_SUPABASE_ANON_KEY")
+
+# if not SUPABASE_URL or not SUPABASE_ANON_KEY:
+#     raise RuntimeError("Supabase env vars not loaded")
+
 import os
 
-load_dotenv()  # load .env file
-
-SUPABASE_URL = os.getenv("NEXT_PUBLIC_SUPABASE_URL")
-SUPABASE_ANON_KEY = os.getenv("NEXT_PUBLIC_SUPABASE_ANON_KEY")
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_ANON_KEY = os.getenv("SUPABASE_ANON_KEY")
 
 if not SUPABASE_URL or not SUPABASE_ANON_KEY:
-    raise RuntimeError("Supabase env vars not loaded")
+    raise RuntimeError("SUPABASE_URL or SUPABASE_ANON_KEY is not set")
